6a12448c3b2eb6414c8b0f3c57760891183bd3d3
Bisma Muqeed <bismamuqeed@gmail.com>
2024-04-02 21:22:48 -0700

    created ATM Machine

---
+++ demo3.js
@@ -0,0 +1,54 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let myBalance = 3000; //Dollaar
+let myPin = 1234;
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        type: "number",
+        message: "enter your pin",
+    },
+]);
+if (pinAnswer.pin === myPin) {
+    console.log("Your Pin Code is Correct  !!! ");
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            type: "list",
+            message: "Please Select The Option:",
+            choices: ["withdraw", "Fast Cash", "Check Balance"],
+        },
+    ]);
+    if (operationAns.operation === "withdraw") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "Enter Your Amount:",
+                type: "number",
+            },
+        ]);
+        if (myBalance >= amountAns.amount) {
+            myBalance -= amountAns.amount;
+            console.log(`Your Remaining Balance is ${myBalance}`);
+        }
+        else {
+            console.log("Your Balance is insufficient");
+        }
+    }
+    else if (operationAns.operation === "Check Balance") {
+        console.log(`Your Current Balance is ${myBalance}`);
+    }
+    else if (operationAns.operation === "Fast Cash") {
+        let fastcash = await inquirer.prompt({
+            name: "cashAmount",
+            type: "list",
+            message: "Select The Amount :",
+            choices: ["100", "1000", "2000", "5000"],
+        });
+        myBalance -= fastcash.cashAmount;
+        console.log(`Your Remaining Balance is ${myBalance}`);
+    }
+}
+else {
+    console.log("Your Pin is incorrect");
+}
